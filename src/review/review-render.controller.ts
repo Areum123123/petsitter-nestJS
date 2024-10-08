9ed9e5615da4f38fedd21537200be21c61ffab38
areum <kang10232@naver.com>
2024-10-08 20:05:40 +0900

    FE:리뷰수정페이

--- src/review/review-render.controller.ts
+++ src/review/review-render.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Patch, Render } from '@nestjs/common';
 
 @Controller('review')
 export class reviewRenderController {
   @Get('my')
   @Render('review/my-review.ejs')
   myReviewPage() {}
+
+  @Get('/edit/:reviewId')
+  @Render('review/edit-review.ejs')
+  editReviewPage() {}
 }
